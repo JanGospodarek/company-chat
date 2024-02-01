81263b61849a58e0da0282f0dd5580f81c53eecb
JanGospodarek <jan.gospo@onet.pl>
2024-02-01 10:37:20 +0100

    tailwind colors

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -2,10 +2,6 @@ import { Card, CardHeader } from "@nextui-org/card";
 
 export default function Home() {
   return (
-    <main>
-      <Card className="w-48" isPressable>
-        <CardHeader>Card Header</CardHeader>
-      </Card>
-    </main>
+    <main className="flex flex-col justify-center items-center gap-4"></main>
   );
 }
--- frontend/tailwind.config.ts
+++ frontend/tailwind.config.ts
@@ -6,7 +6,7 @@ const config: Config = {
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
-    "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}"
+    "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {
@@ -18,6 +18,24 @@ const config: Config = {
     },
   },
   darkMode: "class",
-  plugins: [nextui()],
+  plugins: [
+    nextui({
+      addCommonColors: true,
+
+      themes: {
+        light: {
+          colors: {
+            primary: {
+              DEFAULT: "#8980BD",
+              foreground: "#000000",
+            },
+            secondary: {
+              DEFAULT: "#E5E2F7",
+            },
+          },
+        },
+      },
+    }),
+  ],
 };
 export default config;
