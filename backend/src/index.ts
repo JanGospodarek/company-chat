acc3422df26ab099e3269f1745a5852129e65be2
Dawid Komęza <dawid.komeza@gmail.com>
2024-02-02 09:47:23 +0100

    ✨ Add basic prisma connection

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,15 +1,35 @@
 import express from "express";
 import { Server } from "socket.io";
+import { config } from "dotenv";
+import { PrismaClient } from "@prisma/client";
+import { PrismaClientKnownRequestError } from "@prisma/client/runtime/library";
+
+// Prisma client
+const prisma = new PrismaClient();
+
+// Load environment variables
+config();
 
 const app = express();
 const server = app.listen(5000);
 const io = new Server(server);
 
-app.get("/", (req, res) => {
-  console.log("Hello World!");
-  res.send("Hello World! asdfa");
-});
+app.get("/", async (req, res) => {
+  const users = await prisma.users.findMany();
+  try {
+    await prisma.users.create({
+      data: {
+        username: "test",
+        password: "test",
+      },
+    });
+  } catch (e) {
+    if (e instanceof PrismaClientKnownRequestError && e.code === "P2002") {
+      console.log("Username already exists");
+    }
+  }
 
-app.get("/test", (req, res) => {
-  res.send("test");
+  console.log(users);
+
+  res.send("Hello World! superasdf");
 });
