b3e0ded115a1f145b39f0adee95a0435b8ac5224
JanGospodarek <jan.gospo@onet.pl>
2024-02-07 13:13:31 +0100

    register view

--- mobile/app/auth/login.tsx
+++ mobile/app/auth/login.tsx
@@ -1,32 +1,68 @@
 import { Link } from "expo-router";
+import React from "react";
 import { View, Text, Pressable, StyleSheet } from "react-native";
-import { Button } from "react-native-paper";
+import { Button, TextInput } from "react-native-paper";
 
 const LoginScreen = () => {
+  const [username, setUsername] = React.useState("");
+  const [password, setPassword] = React.useState("");
+
   return (
     <View style={styles.container}>
       <View style={styles.heading}>
         <Text style={{ fontSize: 32, fontWeight: "bold" }}>Login</Text>
       </View>
       <View style={styles.content}>
-        <Link href="/" asChild>
-          <Button
-            mode="outlined"
-            style={{ width: "50%" }}
-            onPress={() => console.log("Pressed")}
-          >
-            <Text>Home </Text>
+        <TextInput
+          label="Username"
+          value={username}
+          style={{ width: 200 }}
+          mode="outlined"
+          onChangeText={(text) => setUsername(text)}
+        />
+        <TextInput
+          label="Password"
+          value={password}
+          style={{ width: 200 }}
+          mode="outlined"
+          onChangeText={(text) => setPassword(text)}
+        />
+        <View style={styles.btnContainer}>
+          <Link href="/" asChild>
+            <Button mode="outlined" onPress={() => console.log("Pressed")}>
+              <Text>Back </Text>
+            </Button>
+          </Link>
+          <Button mode="contained" onPress={() => console.log("Pressed")}>
+            <Text>Log in </Text>
           </Button>
-        </Link>
+        </View>
       </View>
     </View>
   );
 };
 
 const styles = StyleSheet.create({
-  container: { flex: 1, justifyContent: "flex-start" },
-  heading: { flex: 1, flexDirection: "row", justifyContent: "center" },
-  content: { flex: 1, alignItems: "center" },
+  container: { display: "flex", justifyContent: "flex-start" },
+  heading: {
+    display: "flex",
+    flexDirection: "row",
+    justifyContent: "center",
+  },
+  content: {
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center",
+    height: "100%",
+    gap: 10,
+  },
+  btnContainer: {
+    marginTop: 10,
+    display: "flex",
+    flexDirection: "row",
+    justifyContent: "center",
+    gap: 10,
+  },
 });
 
 export default LoginScreen;
