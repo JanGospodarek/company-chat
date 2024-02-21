65a4b12ceb8a5e19e9257d0e1d835bbc23604476
JanGospodarek <jan.gospo@onet.pl>
2024-02-21 17:17:49 +0100

    new chat

--- mobile/app/chat/messages/search.tsx
+++ mobile/app/chat/messages/search.tsx
@@ -1,11 +1,33 @@
-import { View, StyleSheet, Text, TextInput } from "react-native";
+import {
+  View,
+  StyleSheet,
+  Text,
+  TextInput,
+  TouchableOpacity,
+} from "react-native";
 import globalStyles from "@/app/globalStyles";
 import { LinearGradient } from "expo-linear-gradient";
-import { Icon, IconButton, useTheme } from "react-native-paper";
+import { Avatar, Icon, IconButton, useTheme } from "react-native-paper";
 import { useRouter } from "expo-router";
+import { newPrivateChat, newPrivateChatList } from "@/shared/api";
+import React from "react";
+import { User } from "@/shared/types";
 const Search = () => {
   const theme = useTheme();
   const router = useRouter();
+
+  const [newUsers, setNewUsers] = React.useState([] as User[]);
+
+  React.useEffect(() => {
+    newPrivateChatList().then((users) => {
+      setNewUsers(users);
+    });
+  }, []);
+
+  const handleNewChat = async (user: User) => {
+    await newPrivateChat(user.username);
+    router.push("/chat/messages/");
+  };
   return (
     <LinearGradient
       colors={["rgba(137, 128, 189,0.8)", "transparent"]}
@@ -23,15 +45,31 @@ const Search = () => {
           router.push("/chat/messages/");
         }}
       />
-      <Text style={styles.headingText}>Search</Text>
+      <Text style={styles.headingText}>Nowy chat</Text>
       <View style={styles.inputContainer}>
         <Icon source="magnify" color={theme.colors.primary} size={30} />
         <TextInput style={styles.input} placeholder="Type user's name" />
       </View>
       <View style={styles.usersContainer}>
-        <Text style={{ fontFamily: "League-Spartan", fontSize: 18 }}>
+        {/* <Text style={{ fontFamily: "League-Spartan", fontSize: 18 }}>
           Start typing to see users
-        </Text>
+        </Text> */}
+        {newUsers.map((user) => (
+          <TouchableOpacity
+            onPress={() => {
+              handleNewChat(user);
+            }}
+            style={styles.container}
+            key={user.id}
+          >
+            <Avatar.Image
+              size={35}
+              source={require("../../../assets/images/avatar.jpeg")}
+              style={{ margin: 0, padding: 0 }}
+            />
+            <Text style={styles.text}>{user.username}</Text>
+          </TouchableOpacity>
+        ))}
       </View>
     </LinearGradient>
   );
@@ -69,6 +107,22 @@ const styles = StyleSheet.create({
     display: "flex",
     gap: 10,
     justifyContent: "flex-start",
+  },
+  container: {
+    display: "flex",
+    flexDirection: "row",
+    alignItems: "center",
+    gap: 10,
+    padding: 10,
+    borderRadius: 20,
+    marginVertical: 2,
+  },
+  text: {
+    fontFamily: "League-Spartan-SemiBold",
+    fontSize: 20,
+    color: "black",
+    margin: 0,
+    padding: 0,
   },
 });
 export default Search;
