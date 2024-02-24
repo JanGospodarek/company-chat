b5cc3ce680df6c77e2e4b7764af34cb19a023771
Dawid Komęza <dawid.komeza@gmail.com>
2024-02-24 12:20:15 +0100

    ✨ Prepare Docker for production

---
+++ frontend/shared/types.ts
@@ -0,0 +1,39 @@
+export interface User {
+  id: number;
+  username: string;
+  createdAt: string | Date;
+}
+
+interface Chat {
+  chatId: number;
+  name: string;
+  type: "PRIVATE" | "GROUP";
+  createdAt: string | Date;
+  messages: Message[];
+}
+
+export interface PrivateChat extends Chat {
+  receipient: User;
+}
+
+export interface GroupChat extends Chat {
+  users: User[];
+}
+
+export interface Message {
+  chatId: number;
+  messageId: number;
+  content: string;
+  attachment: string | null;
+  createdAt: string | Date;
+  user: User;
+  readBy: User[];
+}
+
+export interface Attachment {
+  id: number;
+  uuid: string;
+  path: string;
+  type: string;
+  name: string;
+}
